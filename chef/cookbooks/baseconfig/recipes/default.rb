6e4ac42790bce46a276587cc23c64492b01e9706
pac6 <pac6@sfu.ca>
2018-03-21 19:16:47 -0700

    Add dotnet core to vm

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -9,7 +9,8 @@ end
 # Base configuration recipe in Chef.
 package "wget"
 package "ntp"
-package "nginx"
+#package "nginx"
+#package "git"
 package "ack-grep"
 cookbook_file "ntp.conf" do
   path "/etc/ntp.conf"
@@ -19,31 +20,53 @@ execute 'ntp_restart' do
 end
 
 # nodejs and npm setup.
-execute 'nodejs_repo_script' do
-  command 'curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -'
-end
-apt_package 'nodejs'
+#execute 'nodejs_repo_script' do
+#  command 'curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -'
+#end
+#apt_package 'nodejs'
 
 # Shell and env variables when logged in.
 # https://stackoverflow.com/a/36896159
-sys_env_file = Chef::Util::FileEdit.new('/etc/environment')
-{
-  'NPM_CONFIG_PREFIX' => '/home/vagrant/.npm-global',
-}.each do |name, val|
-  sys_env_file.insert_line_if_no_match /^#{name}\=/, "#{name}=\"#{val}\""
-  sys_env_file.write_file
-end
-sys_env_file = Chef::Util::FileEdit.new('/home/vagrant/.profile')
-{
-  'export PATH' => '/home/vagrant/.npm-global/bin:$PATH',
-}.each do |name, val|
-  sys_env_file.insert_line_if_no_match /^#{name}\=/, "#{name}=\"#{val}\""
-  sys_env_file.write_file
-end
+#sys_env_file = Chef::Util::FileEdit.new('/etc/environment')
+#{
+#  'NPM_CONFIG_PREFIX' => '/home/vagrant/.npm-global',
+#}.each do |name, val|
+#  sys_env_file.insert_line_if_no_match /^#{name}\=/, "#{name}=\"#{val}\""
+#  sys_env_file.write_file
+#end
+#sys_env_file = Chef::Util::FileEdit.new('/home/vagrant/.profile')
+#{
+#  'export PATH' => '/home/vagrant/.npm-global/bin:$PATH',
+#}.each do |name, val|
+#  sys_env_file.insert_line_if_no_match /^#{name}\=/, "#{name}=\"#{val}\""
+#  sys_env_file.write_file
+#end
 
 # Install Angular.
-execute 'install_angular' do
-  user 'vagrant'
-  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant', 'NPM_CONFIG_PREFIX' => '/home/vagrant/.npm-global'}) 
-  command 'npm install -g @angular/cli'
+#execute 'install_angular' do
+#  user 'vagrant'
+#  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant', 'NPM_CONFIG_PREFIX' => '/home/vagrant/.npm-global'}) 
+#  command 'npm install -g @angular/cli'
+#end
+
+# https://stackoverflow.com/q/40637033
+#npmpath = "/home/vagrant/.npm-global"
+#ENV['PATH'] = "#{ENV['PATH']}:" + npmpath
+#execute 'test_angular' do
+#  command '/home/vagrant/.npm-global/bin/ng new testApp'
+#end
+
+# .NET Core 2.0 installation.
+apt_repository 'dot_net_core2' do
+  uri 'https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod'
+  arch 'amd64'
+  distribution 'xenial'
+  components ['main']
+  key 'https://packages.microsoft.com/keys/microsoft.asc'
+  action :add
 end
+execute 'apt_update' do
+  command 'apt-get update'
+end
+apt_package 'dotnet-sdk-2.1.4'
+
