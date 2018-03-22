f824ca50ca495811a46b3c195b6e8b45c52825d6
pac6 <pac6@sfu.ca>
2018-03-22 00:04:14 -0700

    Add header forwarding for prod

--- CarPoolApp/Startup.cs
+++ CarPoolApp/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,6 +40,11 @@ namespace CarPoolApp
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
+                app.UseAuthentication();
             }
 
             app.UseStaticFiles();
