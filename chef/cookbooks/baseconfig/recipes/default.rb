002e6987ffb4d2bb86896982cd80c81f6ee9a863
pac6 <pac6@sfu.ca>
2018-03-22 17:32:26 -0700

    Optimize chef config

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -18,7 +18,8 @@ end
 execute 'ntp_restart' do
   command 'service ntp restart'
 end
-# Reconfigure nginx for our application.
+
+# Reconfigure nginx as reverse proxy for our application.
 cookbook_file '/etc/nginx/sites-available/default' do
   source 'nginx-carpoolapp'
   action :create
@@ -28,12 +29,10 @@ service 'nginx' do
   action :restart
 end
 
-# nodejs and npm setup.
+# nodejs and npm apt repo setup.
 execute 'nodejs_repo_script' do
   command 'curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -'
 end
-apt_package 'nodejs'
-
 # Shell and env variables when logged in.
 # https://stackoverflow.com/a/36896159
 sys_env_file = Chef::Util::FileEdit.new('/etc/environment')
@@ -51,21 +50,7 @@ sys_env_file = Chef::Util::FileEdit.new('/home/vagrant/.profile')
   sys_env_file.write_file
 end
 
-# Install Angular.
-#execute 'install_angular' do
-#  user 'vagrant'
-#  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant', 'NPM_CONFIG_PREFIX' => '/home/vagrant/.npm-global'}) 
-#  command 'npm install -g @angular/cli'
-#end
-
-# https://stackoverflow.com/q/40637033
-#npmpath = "/home/vagrant/.npm-global"
-#ENV['PATH'] = "#{ENV['PATH']}:" + npmpath
-#execute 'test_angular' do
-#  command '/home/vagrant/.npm-global/bin/ng new testApp'
-#end
-
-# .NET Core 2.0 installation.
+# .net core 2.0 apt repo setup.
 apt_repository 'dot_net_core2' do
   uri 'https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod'
   arch 'amd64'
@@ -74,20 +59,29 @@ apt_repository 'dot_net_core2' do
   key 'https://packages.microsoft.com/keys/microsoft.asc'
   action :add
 end
-execute 'apt_update' do
-  command 'apt-get update'
-end
-apt_package 'dotnet-sdk-2.1.4'
 
-# Build and run project.
+# MS SQL server apt repo setup.
+apt_repository 'ms_sql_server' do
+  uri 'https://packages.microsoft.com/ubuntu/16.04/mssql-server-2017'
+  arch 'amd64'
+  distribution 'xenial'
+  components ['main']
+  key 'https://packages.microsoft.com/keys/microsoft.asc'
+  action :add
+end
+
+# Install required packages to run project.
+apt_package 'nodejs'
+apt_package 'dotnet-sdk-2.1.4'
+apt_package 'mssql-server'
+
+# Configure ms sql server.
+
+# Build and publish project.
 execute 'run_npm_install' do
   command 'npm install'
   cwd '/home/vagrant/CarPoolApp/CarPoolApp'
 end
-#execute 'run_dotnet_build' do
-#  command 'dotnet build --no-incremental'
-#  cwd '/home/vagrant/CarPoolApp/CarPoolApp'
-#end
 execute 'publish_dotnet' do
   command 'dotnet publish --configuration Release --output bin -r linux-x64'
   cwd '/home/vagrant/CarPoolApp/CarPoolApp'
@@ -101,18 +95,7 @@ end
 systemd_unit 'kestrel-carpoolapp.service' do
   action :start
 end
-
-# MS SQL server installation.
-apt_repository 'ms_sql_server' do
-  uri 'https://packages.microsoft.com/ubuntu/16.04/mssql-server-2017'
-  arch 'amd64'
-  distribution 'xenial'
-  components ['main']
-  key 'https://packages.microsoft.com/keys/microsoft.asc'
-  action :add
+execute 'check_app_status' do
+  command 'systemcl status kestrel-carpoolapp.service'
 end
-execute 'apt_update' do
-  command 'apt-get update'
-end
-apt_package 'mssql-server'
 
