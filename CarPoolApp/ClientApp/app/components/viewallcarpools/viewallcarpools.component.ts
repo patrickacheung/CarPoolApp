16ae0324c7ba100d7016b7d276963b6150e58899
Salaar <salaar.rox@hotmail.com>
2018-04-07 02:31:11 -0700

    Worked on adding searching capabilities to the view all list. Added list component to view all.

--- CarPoolApp/ClientApp/app/components/viewallcarpools/viewallcarpools.component.ts
+++ CarPoolApp/ClientApp/app/components/viewallcarpools/viewallcarpools.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Carpool } from '../carpool';
+import { CarpoolService } from '../carpool.service';
+
 
 @Component({
   selector: 'viewallcarpools',
-  templateUrl: './viewallcarpools.component.html'
+    templateUrl: './viewallcarpools.component.html',
+    styleUrls: ['../carpools/carpools.component.css']
 })
+
 export class ViewAllCarpoolsComponent implements OnInit {
-    ngOnInit(){
+
+    carpools: Carpool[];
+
+    constructor(private carpoolService: CarpoolService) { }
+
+    ngOnInit() {
+        this.getCarpools();
+    }
+
+    getCarpools(): void {
+        this.carpoolService.getCarpools()
+            .subscribe(carpools => this.carpools = carpools);
     }
 }
