e86e164fdfa0b20d4cf290dbedfa4d99edf2792c
Salaar <salaar.rox@hotmail.com>
2018-04-07 13:00:18 -0700

    Search tested.

--- CarPoolApp/ClientApp/app/components/mock-carpools.ts
+++ CarPoolApp/ClientApp/app/components/mock-carpools.ts
@@ -1,7 +1,7 @@
 import { Carpool } from './carpool';
 
 export const CARPOOLS: Carpool[] = [
-  { id: 11, driver: 'Mr. Nice', campus: 'Burnaby', vehicle: '1990 Lada', seatsRemaining: 0, datetime: new Date(Date.now()) },
+    { id: 11, driver: 'Mr. Nice', campus: 'Burnaby', vehicle: '1990 Lada', seatsRemaining: 0, datetime: new Date(Date.now()) },
   { id: 12, driver: 'Narco', campus: 'Harbour Centre', vehicle: '1990 Lada', seatsRemaining: 2, datetime: new Date(Date.now())  },
   { id: 13, driver: 'Bombasto', campus: 'Surrey', vehicle: '1990 Lada', seatsRemaining: 1, datetime: new Date(Date.now())  },
   { id: 14, driver: 'Celeritas', campus: 'Burnaby', vehicle: '1990 Lada', seatsRemaining: 3, datetime: new Date(Date.now())  },
