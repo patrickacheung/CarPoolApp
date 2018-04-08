de0363fe263da94ecb2acc6bb63ea1e296f325d2
Salaar <salaar.rox@hotmail.com>
2018-04-07 23:32:15 -0700

    Pull data from webservice for displaying carpools is working.

--- CarPoolApp/ClientApp/app/components/carpool.service.ts
+++ CarPoolApp/ClientApp/app/components/carpool.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule, Http, Headers } from '@angular/http';
 
 import { Carpool } from './carpool';
 import { CARPOOLS } from './mock-carpools';
@@ -8,9 +11,38 @@ import { CARPOOLS } from './mock-carpools';
 @Injectable()
 export class CarpoolService {
 
-  constructor() { }
+    private getCarpoolUrl = 'http://localhost:53381/api/CarPool/Get';
+    private carpools: Carpool[] = [];
+    private testResponse = '';
 
-  getCarpools(): Observable<Carpool[]> {
-    return of(CARPOOLS);
+    constructor(public http: Http) { }
+
+    getCarpools(): Observable<Carpool[]> {
+        var results = '';
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        this.http.get(this.getCarpoolUrl, {
+            headers: headers
+        }).subscribe(
+            data => {
+                console.log(data);
+                var carpoolArray = data.json();
+                for (var i = 0; i < carpoolArray.length; i++) {
+                    var currentCarpool = carpoolArray[i];
+                    this.carpools.push(new Carpool(carpoolArray[i].driver, carpoolArray[i].carDescription, carpoolArray[i].seats, carpoolArray[i].endLocation, carpoolArray[i].time));
+                }
+                
+            });
+
+        console.log("************");
+        console.log(results);
+        console.log("************");
+
+        return of(this.carpools);
+    }
+
+    getCarpoolsCall(): Observable<Carpool[]> {
+        return of(CARPOOLS);
   }
 }
--- CarPoolApp/ClientApp/app/components/carpool.ts
+++ CarPoolApp/ClientApp/app/components/carpool.ts
@@ -1,8 +1,18 @@
 export class Carpool {
+
     id: number;
     driver: string;
     vehicle: string;
     seatsRemaining: number;
     campus: string;
     datetime: Date;
+
+    constructor(driver: string, vehicle: string, seatsRemaining: number, campus: string, datetime: Date) {
+        this.id = -1;
+        this.driver = driver;
+        this.vehicle = vehicle;
+        this.seatsRemaining = seatsRemaining;
+        this.campus = campus;
+        this.datetime = datetime;
+    }
 }
