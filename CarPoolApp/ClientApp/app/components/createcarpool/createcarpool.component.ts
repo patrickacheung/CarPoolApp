fcc8493bc40841b325b7c46afbc3506dd4b32d1d
Patrick Cheung <patrickcheungdev@gmail.com>
2018-04-07 20:33:13 -0700

    Fix padding and text form alignment

--- CarPoolApp/ClientApp/app/components/createcarpool/createcarpool.component.ts
+++ CarPoolApp/ClientApp/app/components/createcarpool/createcarpool.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'createcarpool',
-    templateUrl: './createcarpool.component.html'
+    templateUrl: './createcarpool.component.html',
+    styleUrls: ['./createcarpool.component.css']
 })
 export class CreateCarpoolComponent {
 }
